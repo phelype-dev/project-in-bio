c552c2db1070642f1e9a07df3d0c107b66c6ed61
Phelype Rodrigo Santos <phelype@MacBook-Air-de-Phelype.local>
2025-01-27 22:58:00 -0300

    Finalizado a Hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
 import Image from "next/image";
+import Hero from "@/app/components/lading-page/hero";
 
 export default function Home() {
   return (
-    <h1>Heloo world</h1>
+    <div className={"max-w-7xl mx-auto"}>
+
+      <Hero />
+      {/*<Header />
+
+      <VideoExplanation />
+      <Pricing />
+      <FAQ />*/}
+    </div>
   );
 }
